dbeb9b4ca9dfc04abb63a95aa5fc8642edd4161b
SabrineZekri <63809713+SabrineZekri@users.noreply.github.com>
2022-06-11 18:05:27 +0100

    CommitTest

--- src/main/java/BienEtre_AuTravail/BienEtreAuTravailApplication.java
+++ src/main/java/BienEtre_AuTravail/BienEtreAuTravailApplication.java
@@ -8,6 +8,7 @@ public class BienEtreAuTravailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BienEtreAuTravailApplication.class, args);
+		System.out.println("ok!!");
 	}
 
 }
